8b049bbb36380ca908af6e028a55e2ed4f301f97
Christian Becher <cbecher@stressless-it.de>
2014-08-20 16:11:38 +0200

    Initial commit.

---
+++ web/index.php
@@ -0,0 +1,85 @@
+<?php
+
+    require_once __DIR__.'/../vendor/autoload.php';
+
+    $app = new Silex\Application();
+
+    require_once __DIR__.'/../app/routes/routes.php';
+
+    /*
+     * REGISTERING DOCTRINE DBAL START
+     * */
+    $app -> register(new Silex\Provider\DoctrineServiceProvider(),
+        array
+            (
+                'db.options' => array
+                (
+                    'driver'    => 'pdo_mysql',
+                    'host'      => 'XXXXX',
+                    'dbname'    => 'XXXXX',
+                    'user'      => 'XXXXX',
+                    'password'  => 'XXXXX',
+                    'charset'   => 'utf8'
+                )
+            )
+    );
+    /*
+     * REGISTERING DOCTRINE DBAL END
+     * */
+
+    /*
+     * REGISTERING MONOLOG LOGGER START
+     * */
+    $app -> register(new Silex\Provider\MonologServiceProvider(),
+        array
+        (
+            'monolog.logfile' => __DIR__.'/../app/logfiles/' . date('Y-m-d') . '.txt',
+            'monolog.level' => DEBUG
+        )
+    );
+    /*
+     * REGISTERING MONOLOG LOGGER END
+     * */
+
+    /*
+     * REGISTERING SWIFTMAILER START
+     * */
+    $app -> register(new Silex\Provider\SwiftmailerServiceProvider());
+
+    $app['swiftmailer.options'] = array(
+        'host'       => 'smtp.gmail.com',
+        'port'       => 465,
+        'username'   => 'XXXXX@gmail.com',
+        'password'   => 'XXXXX',
+        'encryption' => 'ssl',
+        'auth_mode'  => 'login'
+    );
+    /*
+     * REGISTERING SWIFTMAILER END
+     * */
+
+    /*
+     * REGISTERING TWIG TEMPLATE ENGINE START
+     * */
+    $app -> register(new Silex\Provider\TwigServiceProvider(),
+        array
+        (
+            'twig.path' => __DIR__.'/../app/views/',
+            'twig.options' => array
+            (
+                'debug' => true,
+                'charset' => 'utf-8',
+                'cache' => __DIR__.'/../app/cache/',
+                'auto_reload' => 'true',
+                'strict_variables' => true,
+                'autoescape' => true,
+                'optimizations' => -1
+            )
+        )
+    );
+
+    /*
+     * REGISTERING TWIG TEMPLATE ENGINE END
+     * */
+    
+    $app -> run();
